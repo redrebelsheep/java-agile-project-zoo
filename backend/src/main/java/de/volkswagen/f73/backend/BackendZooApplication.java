fa2cf254959884057d8cba9b4696b10a22df8f41
Kowall, Marian <marian.kowall@volkswagen.de>
2022-05-29 14:43:43 +0200

    adding localapi use via CrosFilter

--- backend/src/main/java/de/volkswagen/f73/backend/BackendZooApplication.java
+++ backend/src/main/java/de/volkswagen/f73/backend/BackendZooApplication.java
@@ -15,8 +15,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Set;
 
 
@@ -35,6 +46,8 @@ public class BackendZooApplication implements CommandLineRunner {
     public static void main(String[] args) throws JsonProcessingException {
         SpringApplication.run(BackendZooApplication.class, args);
     }
+
+
 
     @Override
     public void run(String... args) throws Exception {
--- backend/src/main/java/de/volkswagen/f73/backend/config/GlobalBeansConfiguration.java
+++ backend/src/main/java/de/volkswagen/f73/backend/config/GlobalBeansConfiguration.java
@@ -10,6 +10,12 @@ import de.volkswagen.f73.backend.enclosure.EnclosureMapper;
 import de.volkswagen.f73.backend.stall.StallMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class GlobalBeansConfiguration {
@@ -19,6 +25,23 @@ public class GlobalBeansConfiguration {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         return objectMapper;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+//        corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+        corsConfiguration.setAllowedOrigins(Collections.singletonList("http://127.0.0.1:5500"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+                                                          "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+                                                          "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+                                                          "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
     }
 
     @Bean
